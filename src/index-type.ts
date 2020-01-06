28a60e5fa5197b9fbdc7c7e7837245775d9420d0
shimkijun <shimkijun@naver.com>
2020-01-06 12:32:14 +0900

    add index-type

--- src/index-type.ts
+++ src/index-type.ts
@@ -1,7 +1,38 @@
 interface Props {
+    name : string
     [key: string] : string;
 }
 
 const p: Props = {
-    a: 'd'
+    name : 'dog',
+    a : 'a',
+    b : 'b',
+    c : 'c',
+    0 : '3',
+    1 : 'd',
+    2 : 'e'
+}
+
+p[0];
+p['a'];
+p.a;
+p.name;
+p.abc;
+p.ddd;
+
+
+let keys : keyof Props;
+
+interface User {
+    name : string;
+    age : number;
+    hello(msg : string) : void;
+}
+
+let keysOfUser : keyof User;
+
+let helloMethod : User["hello"];
+
+helloMethod = function hello(msg : string){
+
 }
